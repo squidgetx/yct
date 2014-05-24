34040c6739a75e03d90e2accf3061acd575cefbb
Sylvan Zheng <squidgetx@gmail.com>
2014-05-24 12:04:11 -0400

    fleshing out climber controller

--- app/controllers/climbers_controller.rb
+++ app/controllers/climbers_controller.rb
@@ -1,8 +1,28 @@
 class ClimbersController < ApplicationController
-	def show
-		@climber = Climber.find(params[:id])
-	end
+	before_action :find_climber, [:show, :edit, :destroy]
+	
 	def index
 		@climbers = Climber.all
 	end
+	def show
+		
+	end
+	def edit
+
+	end
+	def create
+		@climber = Climber.create(climb_params)
+		redirect_to(@climber)
+	end
+	def destroy
+	
+	end
+	
+	def find_climber
+		@climber = Climber.find(params[:id])
+	end
+	def climb_params
+		params.required(:climber).permit(:name, :state)
+	end
+	
 end
