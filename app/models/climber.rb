4e1e47217b7d5988c5bfef1bb610d62f14b3b1a5
squidgetx <squidgetx@gmail.com>
2014-07-12 13:34:02 -0400

    change to postgresql

--- app/models/climber.rb
+++ app/models/climber.rb
@@ -1,5 +1,5 @@
 class Climber < ActiveRecord::Base
-	has_many :posts
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_many :posts
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602211111) do
+ActiveRecord::Schema.define(version: 20140606200043) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "climbers", force: true do |t|
     t.string   "name"
@@ -24,6 +27,11 @@ ActiveRecord::Schema.define(version: 20140602211111) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+  end
+
+  create_table "post_images", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
