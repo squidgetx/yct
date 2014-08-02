7174022127547cc814af4ed7d100ec0f87de8dc1
squidgetx <squidgetx@gmail.com>
2014-08-02 14:56:30 -0400

    use pry instead of irb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
+
   Paperclip.options[:command_path] = "/usr/local/bin/"
+  silence_warnings do
+    begin
+      require 'pry'
+      IRB = Pry
+    rescue LoadError
+    end
+  end
 end
