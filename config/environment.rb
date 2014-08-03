16b98555eb475861a22f2556b74e9aa72b972276
squidgetx <squidgetx@gmail.com>
2014-08-03 18:21:09 -0400

    fix cas authentication

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
-Rails.application.initialize!
 
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => "https://secure.its.yale.edu/cas/"
 )
+
+Rails.application.initialize!
