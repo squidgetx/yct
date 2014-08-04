9de1067c5fe41771647a4eb3dca24606aac0833a
squidgetx <squidgetx@gmail.com>
2014-08-04 00:07:15 -0400

    laying down more view groundwork

--- app/controllers/climbers_controller.rb
+++ app/controllers/climbers_controller.rb
@@ -1,48 +1,44 @@
 class ClimbersController < ApplicationController
-	#require 'casclient'
-	#require 'casclient/frameworks/rails/filter'
-	before_filter CASClient::Frameworks::Rails::GatewayFilter
+  before_action :find_climber, only: [:show, :edit, :destroy, :update]
 
-	before_action :find_climber, only: [:show, :edit, :destroy, :update]
-	
-	def index
-		@climbers = Climber.all
-		if session[:cas_user]
-			@user = session[:cas_user]
-		end
-	end
-	def show
-		
-	end
-	def edit
-		@oldDesc = @climber.desc
-		@oldName = @climber.name
-		@method = 'put'
-		@path = climber_path(@climber)
-		render "new"
-	end
-	def new
-		@method = 'post'
-		@path = climbers_path
-	end
-	def create
-		@climber = Climber.create(climb_params)
-		redirect_to(@climber)
-	end
-	def update
-		Climber.update(@climber.id, climb_params)
-		redirect_to(@climber)
-	end
-	def destroy
-		@climber.destroy
-		redirect_to :action => 'index'
-	end
-	
-	def find_climber
-		@climber = Climber.find(params[:id])
-	end
-	def climb_params
-		params.required(:climber).permit(:name, :college, :year, :desc, :avatar)
-	end
-	
+  def index
+    @climbers = Climber.all
+    if session[:cas_user]
+      @user = session[:cas_user]
+    end
+  end
+  def show
+
+  end
+  def edit
+    @oldDesc = @climber.desc
+    @oldName = @climber.name
+    @method = 'put'
+    @path = climber_path(@climber)
+    render "new"
+  end
+  def new
+    @method = 'post'
+    @path = climbers_path
+  end
+  def create
+    @climber = Climber.create(climb_params)
+    redirect_to(@climber)
+  end
+  def update
+    Climber.update(@climber.id, climb_params)
+    redirect_to(@climber)
+  end
+  def destroy
+    @climber.destroy
+    redirect_to :action => 'index'
+  end
+
+  def find_climber
+    @climber = Climber.find(params[:id])
+  end
+  def climb_params
+    params.required(:climber).permit(:name, :college, :year, :desc, :avatar)
+  end
+
 end
