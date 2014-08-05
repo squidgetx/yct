76d85085f24e52f386e8e1973a2feb46db6c29cf
squidgetx <squidgetx@gmail.com>
2014-08-05 15:51:28 -0400

    fix paperclip path

--- app/models/climber.rb
+++ app/models/climber.rb
@@ -1,8 +1,10 @@
 class Climber < ActiveRecord::Base
   has_many :posts
   has_and_belongs_to_many :photos
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def self.guest
     # return a dummy climber
