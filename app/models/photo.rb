590e6ce7066abbda398a515a2be49b691ec2917a
squidgetx <squidgetx@gmail.com>
2014-08-05 16:45:11 -0400

    sort of good climber page?

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   has_and_belongs_to_many :posts
   has_and_belongs_to_many :climbers
-  has_attached_file :image, styles: { medium: "512x512" }, default_url: "/images/:style/missing.png", :path => ":rails_root/public/system/:attachment/:id/:style/:filename", :url => "/system/:attachment/:id/:style/:filename"
+  has_attached_file :image, styles: { medium: "512x512" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   scope :covers, lambda { where('cover = true') }
 end
