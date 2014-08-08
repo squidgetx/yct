417b12f61d7f681079344e8eebce7c280df3b92a
squidgetx <squidgetx@gmail.com>
2014-08-08 13:30:58 -0400

    scaffold events

--- db/migrate/20140808172647_create_events.rb
+++ db/migrate/20140808172647_create_events.rb
@@ -1,11 +1,13 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+
       t.string :name
       t.text :description
       t.datetime :start_date
       t.datetime :end_date
       t.boolean :signup
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808172415) do
+ActiveRecord::Schema.define(version: 20140808172647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20140808172415) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.boolean  "signup"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "photos", force: true do |t|
