f88b79a41b689facdb9b07a153287a2b04035eee
squidgetx <squidgetx@gmail.com>
2014-08-08 14:01:16 -0400

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+8.times do
+Climber.create! do |climber|
+  climber.name = "Riley Rice"
+  climber.role = 'normal'
+  climber.login = 'some login'
+  climber.avatar = File.open(Dir.glob(File.join(Rails.root, 'db', 'seed_images', '*')).sample)
+end
+end
+
+8.times do
+  Post.create! do |post|
+    post.title = "A post"
+    post.text = ''
+    [1,2,3,4,5,6].sample.times do
+      post.text += 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n'
+    end
+    post.climber_id = 1;
+  end
+end
