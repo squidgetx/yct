62724020534119a30e81562704b649b11633a8e0
squidgetx <squidgetx@gmail.com>
2014-08-08 17:52:50 -0400

    scaffolding for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   resources :events
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, :type => :controller do
+
+  describe "GET about" do
+    it "returns http success" do
+      get :about
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to be_success
+    end
+  end
+
+end
