dec4d2ee1dcaf14d1804fd6f46f4ab58f3cb5afe
squidgetx <squidgetx@gmail.com>
2014-08-09 13:35:55 -0400

    no longer crashes on posts when not logged in

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.nil? || user.id.nil?
+      # guest user
+
+      can :read, Post
+      return
+    end
     case user.role
       when 'admin'
         can :manage, :all
