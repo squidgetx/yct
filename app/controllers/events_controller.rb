28de11ab742d6970ee7a31bb18bd36fb7eb5ca36
squidgetx <squidgetx@gmail.com>
2014-08-15 16:57:01 -0400

    event gui

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,15 +25,10 @@ class EventsController < ApplicationController
   # POST /events.json
   def create
     @event = Event.new(event_params)
-
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -69,6 +64,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params[:event]
+      params.required(:event).permit(:name, :description, :start_date, :end_date, :signup)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
 class Event < ActiveRecord::Base
+  validates :name, :start_date, :end_date, presence: true
+
+  def get_range
+    # Return human readable string of the event time
+    start_date = self.start_date.to_date(:long)
+    end_date = self.end_date.to_date.to_s(:long)
+    start_time = Time.at(self.start_date).strftime('%R')
+    end_time = Time.at(self.end_date).strftime('%R')
+    if start_date == end_date
+      # the event is a single day event
+      return "#{start_date} #{start_time} - #{end_time}"
+    else
+      # the event spans multiple days
+      return "#{start_date} #{start_time} - #{end_date} #{end_time}"
+    end
+  end
 end
