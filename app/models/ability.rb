341b3b8bc0a7a7c4040a4e5bdf9f984bbe0c8ce2
squidgetx <squidgetx@gmail.com>
2014-08-21 15:29:19 -0400

    fix bug where moderators, admins could sign up by accident

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,9 +16,17 @@ class Ability
     case user.role
       when 'admin'
         can :manage, :all
+        cannot :signup, Event
+        can :signup, Event do |event|
+          event.start_date.to_date <= Date.current &&
+            event.signup &&
+            event.climbers.where("climber_id = ?",user.id).empty?
+        end
+
       when 'moderator'
         can :manage, Post
         can :manage, Event
+        cannot :signup, Event
         can :manage, Climber
       when 'normal' || 'moderator'
         can [:read, :create], Post
